bf21bb8f78ba3f01012ac63b8d1cb4c39b7e8432
YeaminFaiaj <76264980+YeaminFaiaj@users.noreply.github.com>
2022-12-13 15:00:49 +0600

    Add files via upload

---
+++ bspwm/themes/current.bash
@@ -0,0 +1,34 @@
+# -----------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+# Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+#
+# Blade color scheme
+# -----------------------------------------------------------------------------
+
+# Wallpaper
+wallpaper='/home/yeamin/Pictures/wallpapers/amerana.jpg'
+
+# Special
+foreground='#FFFFFF'
+background='#1A2020'
+cursor='#FFFFFF'
+
+# Colors
+color0='#242D35'
+color1='#FB6396'
+color2='#94CF95'
+color3='#e5c76b'
+color4='#6EC1D6'
+color5='#CD84C8'
+color6='#7FE4D2'
+color7='#FFFFFF'
+color8='#526170'
+color9='#F92D72'
+color10='#6CCB6E'
+color11='#f4d67a'
+color12='#4CB9D6'
+color13='#C269BC'
+color14='#58D6BF'
+color15='#F4F5F2'
+
+# Don't Delete This File
